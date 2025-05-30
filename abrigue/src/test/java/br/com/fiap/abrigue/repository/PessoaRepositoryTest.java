807d114a970a020db2590c3adecbf608e8c432ee
Luna Faustino <lu.faustino2000@gmail.com>
2025-05-30 20:11:33 -0300

    adiciona suporte a perfis de teste nas classes de teste do controlador e repositório

--- abrigue/src/test/java/br/com/fiap/abrigue/repository/PessoaRepositoryTest.java
+++ abrigue/src/test/java/br/com/fiap/abrigue/repository/PessoaRepositoryTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,7 +16,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@TestPropertySource(locations = "classpath:application.properties")
+@ActiveProfiles("test")
 class PessoaRepositoryTest {
 
     @Autowired
@@ -119,4 +119,3 @@ class PessoaRepositoryTest {
         assertEquals(3L, contagem);
     }
 }
-
